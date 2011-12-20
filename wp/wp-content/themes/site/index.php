cf3c739418b0aea124475a1be6b15e18395685e2
eric <eric@xtunes.cc>
2011-12-20 14:33:33 +0800

    添加搜索

--- wp/wp-content/themes/site/index.php
+++ wp/wp-content/themes/site/index.php
@@ -14,19 +14,13 @@
  */
 
 get_header(); ?>
-
-		<div id="container">
-			<div id="content" role="main">
-
-			<?php
-			/* Run the loop to output the posts.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-index.php and that will be used instead.
-			 */
-			 get_template_part( 'loop', 'index' );
-			?>
-			</div><!-- #content -->
-		</div><!-- #container -->
-
-<?php get_sidebar(); ?>
+ <div id="main" class="wrap">
+    	<?php get_sidebar(); ?>
+    	<div class="rightpart">
+    		<div class="content">
+    		    <h1>请输入搜索内容</h1>
+			</div>
+    	</div>
+    	<div class="clear"></div>
+    </div>
 <?php get_footer(); ?>
--- wp/wp-content/themes/site/search.php
+++ wp/wp-content/themes/site/search.php
@@ -8,30 +8,26 @@
  */
 
 get_header(); ?>
-
-		<div id="container">
-			<div id="content" role="main">
-
-<?php if ( have_posts() ) : ?>
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyten' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-				<?php
-				/* Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called loop-search.php and that will be used instead.
-				 */
-				 get_template_part( 'loop', 'search' );
-				?>
-<?php else : ?>
+<div id="main" class="wrap">
+    	<?php get_sidebar(); ?>
+    	<div class="rightpart">
+    		<div class="content">
+    			<?php if ( have_posts() ) : ?>
+    			    		    		<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyten' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+    		  <ul class="newslist">
+    		  	<li><a href="<?php the_permalink() ?>" target="_top"><?php the_title(); ?></a></li>
+    		  </ul>
+    		  <?php else : ?>
 				<div id="post-0" class="post no-results not-found">
-					<h2 class="entry-title"><?php _e( 'Nothing Found', 'twentyten' ); ?></h2>
+					<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyten' ); ?></h1>
 					<div class="entry-content">
-						<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyten' ); ?></p>
-						<?php get_search_form(); ?>
+						<div><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyten' ); ?></div>
 					</div><!-- .entry-content -->
 				</div><!-- #post-0 -->
 <?php endif; ?>
-			</div><!-- #content -->
-		</div><!-- #container -->
-
-<?php get_sidebar(); ?>
+			</div>
+    	</div>
+    	<div class="clear"></div>
+    </div>
 <?php get_footer(); ?>
+	
